f1ba6bec48b851d9db89d435e56ad3e2aaf10d80
snakpak <mike.pakulak@gmail.com>
2021-01-29 12:18:03 -0500

    Seeded db and working on controllers

---
+++ app/controllers/vineyards_controller.rb
@@ -0,0 +1,49 @@
+class VineyardsController < ApplicationController
+
+
+    #GET /vineyards
+    def index
+        @vineyards = Vineyard.all
+
+        render json: @vineyards
+    end
+
+    #GET /vineyards/1
+    def show
+        render json: @food, include: :wines
+    end
+
+    #POST /vineyards
+    def create
+        @vineyards = Vineyard.new(vineyard_params)
+        @vineyard.user = @current_user
+    end
+
+    #PATCH/PUT /vineyards/1
+    def update
+        @vineyard = @current_user.vineyards.find(params[:id])
+
+        if @vineyard.update(vineyard_params)
+            render json: @vineyard
+        else
+            render json: @vineyard.errors, status: :unprocessable_entity
+        end
+    end
+
+    #DELETE /vineyards/1
+    def destroy
+        @vineyard = @current_user.vineyards.find(params[:id])
+        @vineyard.destroy
+    end
+
+
+    private
+
+    def set_vineyard
+        @vineyard = Vineyard.find(params[:id])
+    end
+
+    def vineyard_params
+        params.require(:vineyard).permit(:name)
+    end
+end
---
+++ app/controllers/wines_controller.rb
@@ -0,0 +1,35 @@
+class WinesController < ApplicationController
+
+    def index
+        
+    end
+
+
+    def show
+
+    end
+
+
+    def create
+    
+    end
+
+
+    def update
+
+    end
+
+
+    def destroy
+
+    end
+
+
+    private
+
+    def wine_params
+    end
+
+
+
+end
