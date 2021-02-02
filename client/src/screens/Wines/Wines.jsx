f8b2118f650a676a6bf45648e60a93363c84a1b6
snakpak <mike.pakulak@gmail.com>
2021-02-01 22:24:57 -0500

    Layout of wines list

--- client/src/screens/Wines/Wines.jsx
+++ client/src/screens/Wines/Wines.jsx
@@ -1,20 +1,36 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {NavLink} from 'react-router-dom';
-
+import './Wines.css';
 
 
 export default function Wines(props) {
     const { wines, currentUser } = props;
 
 
+
     return (
-        <div className='extreme_fuckery'>
+        <div className='wine_page'>
+            <div className='wines_table_header'>
+                <h4>Vintage</h4>
+                <h4>Name</h4>
+                <h4>Category</h4>
+                <h4>Variety</h4>
+                <h4>Price Min</h4>
+                <h4>Price Max</h4>                
+                <h4>Size</h4>
+            </div>
             {wines.map(wine=> (
-                <React.Fragment key={wine.id}>
+                <div className='wines' key={wine.id}>
+                        <p className='vintage'>{wine.vintage}</p>
                     <NavLink to={`/wine/${wine.id}`}>
-                        <p>{wine.name}</p>
+                        <p className='wine_name'>{wine.name}</p>                        
                     </NavLink>
-                </React.Fragment>
+                    <p className='category'>{wine.category}</p>
+                        <p className='variety'>{wine.varietal}</p>
+                        <p className='min_price'>$ {wine.price_min}</p>
+                        <p className='max_price'>$ {wine.price_max}</p>
+                        <p className='size'>{wine.size} mL</p>
+                </div>
             ))}
         </div>
     )
