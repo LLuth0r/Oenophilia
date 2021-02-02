4ed7f8d36054763747a37481c08c808bd16914f2
snakpak <mike.pakulak@gmail.com>
2021-02-02 12:21:59 -0500

    Added production env

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3000'
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://git.heroku.com/the-cellar-api.git' : 'http://localhost:3000'
 
 const api = axios.create({
     baseURL: baseURL
