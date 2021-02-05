bedaa3b5ea998085f3f4f1d06fd65302018101ae
snakpak <mike.pakulak@gmail.com>
2021-02-05 11:37:19 -0500

    Updated api config to the right heroku link

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === 'production' ? 'https://git.heroku.com/the-cellar-api.git' : 'http://localhost:3000'
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://the-cellar-api.herokuapp.com' : 'http://localhost:3000'
 
 const api = axios.create({
     baseURL: baseURL
